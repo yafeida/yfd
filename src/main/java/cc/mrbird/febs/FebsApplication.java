d3ceba487325c03da0edaf02680af1b3df3dde1e
xiexiaozhen <1310993083@qq.com>
2019-05-29 14:38:48 +0800

    a

--- src/main/java/cc/mrbird/febs/FebsApplication.java
+++ src/main/java/cc/mrbird/febs/FebsApplication.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAsync
 public class FebsApplication {
     public static void main(String[] args) {
+    	
         new SpringApplicationBuilder(FebsApplication.class)
                 .run(args);
     }
